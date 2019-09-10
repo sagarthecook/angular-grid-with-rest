47fbe9935ba3c93cbbf64f6ae520b03d6637a085
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-09-10 17:34:14 +0530

    first commit

---
+++ service-providers/src/main/java/com/sp/serviceproviders/ServiceProvidersApplication.java
@@ -0,0 +1,13 @@
+package com.sp.serviceproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProvidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProvidersApplication.class, args);
+	}
+
+}
