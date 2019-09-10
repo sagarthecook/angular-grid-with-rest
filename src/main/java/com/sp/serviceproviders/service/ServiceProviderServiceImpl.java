710ef91734028952883f718d04bcf0341986b769
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-09-10 23:31:57 +0530

    update : Static folder in app

--- src/main/java/com/sp/serviceproviders/service/ServiceProviderServiceImpl.java
+++ src/main/java/com/sp/serviceproviders/service/ServiceProviderServiceImpl.java
@@ -13,7 +13,17 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
 	@Override
 	public List<ServiceProvider> findAllServiceProviders() throws Exception {
 		List<ServiceProvider> providers = new ArrayList<ServiceProvider>();
-		providers.add(new ServiceProvider("Idea", "150MB", "Idea providers", "8888688944", "sagarjdhv968@gmail.coom",
+		providers.add(new ServiceProvider("Idea", "150MB", "Idea providers", "8888688944", "idea@idea.coom",
+				"www.idea.com", 100, 4.5f));
+		providers.add(new ServiceProvider("Vodafone", "200MB", "Vodafone providers", "1987542163", "customer@Vodafone.coom",
+				"www.idea.com", 200, 4.5f));
+		providers.add(new ServiceProvider("Jio", "250MB", "Jio providers", "7458963210", "jio@jio.coom",
+				"www.idea.com", 100, 4.5f));
+		providers.add(new ServiceProvider("Airtel", "150MB", "Airtel providers", "7458691235", "airtel@airtel.coom",
+				"www.idea.com", 100, 4.5f));
+		providers.add(new ServiceProvider("BSNL", "160MB", "BSNL providers", "8888688944", "bsnl@bsnl.coom",
+				"www.idea.com", 100, 4.5f));
+		providers.add(new ServiceProvider("MTNL", "120MB", "MTNL providers", "9865485475", "mtnl@mtnl.coom",
 				"www.idea.com", 100, 4.5f));
 		return providers;
 	}
